3ab4882218d95a130d54f331066cc63a46902a17
metatech-programmer <97856988+metatech-programmer@users.noreply.github.com>
2024-01-13 14:33:28 -0500

    links

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import links from "./links/links.json";
+import LinkMidu from "./components/links-midu/LinksMidu";
+
+import "./App.css";
+
+function App() {
+	return (
+		<main className=" w-screen h-screen bg-zinc-950 pt-10 overflow-auto">
+			<h1 className="font-bold text-zinc-300 text-7xl text-center">
+				Recursos Dev
+			</h1>
+			<hr className="w-4/5 mx-auto mt-10 border-gray-600" />
+			<div className=" bg-zinc-900 p-32 text-slate-100 flex flex-wrap justify-center items-center gap-3">
+				<LinkMidu jsonData={links} />
+			</div>
+		</main>
+	);
+}
+
+export default App;
---
+++ src/components/links-midu/LinksMidu.jsx
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from "react";
+import barcelona from "../../assets/thumbnails/candidatura.42barcelona.webp";
+const LinkMidu = ({ jsonData }) => {
+	const medidas = [30, 40, 50, 60];
+	return (
+		<>
+			{jsonData.links.map((enlace, index) => (
+				<a
+					className={`relative w-[${
+						medidas[Math.floor(Math.random() * 4)]
+					}%] h-72 border break-words text-balance overflow-hidden  hover:bg-sky-900 hover:shadow-sky-400 shadow-md hover:scale-105 z-0 hover:z-10 transition-all rounded-lg active:scale-110 active:shadow-sky-600`}
+					// biome-ignore lint/suspicious/noArrayIndexKey: <explanation>
+					key={index}
+					href={enlace}
+					target="_blank"
+					rel="noopener noreferrer"
+					style={{
+						backgroundImage: `url(
+						src/assets/thumbnails/${jsonData.thumbnails[index]})`,
+						backgroundSize: "cover",
+						backgroundPosition: "left",
+					}}
+				>
+					<span>
+						<span className="text-3xl font-semibold hover:bg-black opacity-5 hover:opacity-40 w-full h-full flex items-center justify-center p-5 ">
+							{new URL(enlace).hostname}
+						</span>
+					</span>
+				</a>
+			))}
+		</>
+	);
+};
+
+export default LinkMidu;
