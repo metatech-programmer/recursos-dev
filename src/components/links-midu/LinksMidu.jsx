ae46e7dce3e65f8d678f631556520c21d2e66ca1
metatech-programmer <97856988+metatech-programmer@users.noreply.github.com>
2024-01-14 01:13:56 -0500

    update images

--- src/components/links-midu/LinksMidu.jsx
+++ src/components/links-midu/LinksMidu.jsx
@@ -2,14 +2,11 @@ import React from "react";
 
 const LinkMidu = ({ jsonData }) => {
 	const medidas = ["30", "40", "50", "60"];
-
 	return (
 		<>
 			{jsonData.links.map((enlace, index) => (
 				<a
-					className={`w-[${
-						medidas[Math.floor(Math.random() * 4)]
-					}%] h-72 border break-words text-balance overflow-hidden  hover:bg-sky-900 hover:shadow-sky-400 shadow-md hover:scale-105 z-0 hover:z-10 transition-all rounded-lg active:scale-110 active:shadow-sky-600`}
+					className="h-72 border break-words text-balance overflow-hidden  hover:bg-sky-900 hover:shadow-sky-400 shadow-md hover:scale-105 z-0 hover:z-10 transition-all rounded-lg active:scale-110 active:shadow-sky-600 w-full md:w-auto"
 					// biome-ignore lint/suspicious/noArrayIndexKey: <explanation>
 					key={index}
 					href={enlace.link} 
@@ -22,7 +19,7 @@ const LinkMidu = ({ jsonData }) => {
 					}}
 				>
 					<span>
-						<span className="text-3xl font-semibold hover:bg-black opacity-5 hover:opacity-40 w-full h-full flex items-center justify-center p-5 ">
+						<span className="text-3xl font-semibold md:hover:bg-black bg-black md:bg-transparent opacity-40 md:opacity-5 md:hover:opacity-40 w-full h-full flex items-center justify-center p-5 ">
 							{new URL(enlace.link).hostname}
 						</span>
 					</span>
