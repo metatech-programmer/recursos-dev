97190c40c2443891ed18e59e8c0fbc8b91b1bb46
metatech-programmer <97856988+metatech-programmer@users.noreply.github.com>
2024-01-14 02:05:29 -0500

    update css

--- src/App.jsx
+++ src/App.jsx
@@ -6,15 +6,22 @@ import "./App.css";
 
 function App() {
 	return (
-		<main className="w-screen h-screen bg-zinc-950 pt-10 overflow-hidden ">
-			<h1 className="font-bold text-zinc-300 text-3xl md:text-7xl text-center">
-				Recursos Dev
-			</h1>
-			<hr className="w-4/5 mx-auto mt-10 border-gray-600" />
-			<div className=" bg-zinc-900 p-8 sm:p-12 md:p-32 text-slate-100 flex flex-wrap justify-center items-center gap-3 overflow-y-scroll w-full h-full ">
-				<LinkMidu jsonData={links} />
-			</div>
-		</main>
+		<>
+				<div id="top" />
+			<main className="w-screen h-auto bg-gradient-to-b from-zinc-950 to-100% to-zinc-800 pt-10">
+				<h1 className="font-bold text-zinc-300 text-3xl md:text-5xl text-center ">
+					&#128526; &middot; Recursos Dev &middot; &#128526;
+				</h1>
+				<hr className="w-11/12 mx-auto mt-10 border-zinc-900" />
+				<div className="  p-8 sm:p-12 md:p-32 text-slate-100 flex flex-wrap justify-center items-center gap-3 overflow-y-scroll w-full h-full">
+					<LinkMidu jsonData={links} />
+				</div>
+
+				<a id="back-top" href="#top">
+					UP
+				</a>
+			</main>
+		</>
 	);
 }
 
