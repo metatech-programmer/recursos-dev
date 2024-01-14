87ad0d3550a792874c40a717f492daec5e55e143
metatech-programmer <97856988+metatech-programmer@users.noreply.github.com>
2024-01-14 00:36:41 -0500

    Update images

--- src/components/links-midu/LinksMidu.jsx
+++ src/components/links-midu/LinksMidu.jsx
@@ -1,7 +1,8 @@
-import React, { useState, useEffect, useContext } from "react";
+import React from "react";
 
 const LinkMidu = ({ jsonData }) => {
 	const medidas = ["30", "40", "50", "60"];
+
 	return (
 		<>
 			{jsonData.links.map((enlace, index) => (
@@ -11,18 +12,18 @@ const LinkMidu = ({ jsonData }) => {
 					}%] h-72 border break-words text-balance overflow-hidden  hover:bg-sky-900 hover:shadow-sky-400 shadow-md hover:scale-105 z-0 hover:z-10 transition-all rounded-lg active:scale-110 active:shadow-sky-600`}
 					// biome-ignore lint/suspicious/noArrayIndexKey: <explanation>
 					key={index}
-					href={enlace}
+					href={enlace.link} 
 					target="_blank"
 					rel="noopener noreferrer"
 					style={{
-						backgroundImage: `url(${(`/thumbnails/${jsonData.thumbnails[index]}`)})`,
-						backgroundSize: "cover", 
+						backgroundImage: `url(${`/thumbnails/${jsonData.links[index].thumbnail}.webp`})`,
+						backgroundSize: "cover",
 						backgroundPosition: "left",
 					}}
 				>
 					<span>
 						<span className="text-3xl font-semibold hover:bg-black opacity-5 hover:opacity-40 w-full h-full flex items-center justify-center p-5 ">
-							{new URL(enlace).hostname}
+							{new URL(enlace.link).hostname}
 						</span>
 					</span>
 				</a>
